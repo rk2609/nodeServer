60ccd8118cadee182f6cf14a72c3f856a0dd3805
rk2609 <accounts@wanderon.in>
2020-03-13 17:07:48 +0530

    more debugging

--- server.js
+++ server.js
@@ -27,7 +27,18 @@ app.use(cors());
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.json('abc')
+	db.select('*')
+	.from('users')
+	.then(user => {
+		if(user.length) {
+			res.render('home', {users: user});
+			//console.log(user);
+			//res.json(user);	
+		} else {
+			res.status(400).json('User Not Found');
+		}
+	})
+	.catch(err => res.status(400).json('error getting user'))
 });
 app.post('/signin', signin.handleSignIn(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
