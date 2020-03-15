320a656ecae133aaeebe6d2f9b255f28b253ddb9
rk2609 <accounts@wanderon.in>
2020-03-15 22:19:58 +0530

    updating year & month

---
+++ controllers/updatecolumns.js
@@ -0,0 +1,19 @@
+const handleUpdateColumns = (db) => (req, res) => {
+	const  column = 'Submission_Date';
+	db('sales_final')
+	.distinct('' + column)
+	.then(user => {
+		if(user.length) {
+			//res.render('home', {users: user});
+			//console.log(user);
+			res.json(user);	
+		} else {
+			res.status(400).json('User Not Found');
+		}
+	})
+	.catch(err => res.status(400).json('error getting user'))
+}
+
+module.exports = {
+	handleUpdateColumns: handleUpdateColumns
+};
