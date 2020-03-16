382e75188782d852ae5d712eb2b8f184095e0b22
rk2609 <accounts@wanderon.in>
2020-03-16 14:19:14 +0530

    had some error

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get('/sales/:column', salesdistinct.handleSalesDistinct(db));
 
 //const PORT = process.env.PORT;
 
-app.listen(/*process.env.PORT || */3000, () => {
-	console.log(`Server is listening on port `)
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`Server is listening on port ${process.env.PORT}`)
 });
